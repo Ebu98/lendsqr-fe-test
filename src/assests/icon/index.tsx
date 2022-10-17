d25c295b49fa69e67fe8d087b4aabb505102cca3
Ebu98 <91473642+Ebu98@users.noreply.github.com>
2022-10-17 18:28:49 +0100

    updated the readme file

--- src/assests/icon/index.tsx
+++ src/assests/icon/index.tsx
@@ -9,28 +9,3 @@ export { ReactComponent as StarIcon } from "./star.svg";
 
 export { ReactComponent as LogoIcon } from "./logo.svg";
 export { ReactComponent as BellIcon } from "./bell.svg";
-
-
-// export { ReactComponent as SwitchIcon } from "./swtch.svg";
-// export { ReactComponent as DashboardIcon } from "./dashboard.svg";
-// export { ReactComponent as UsersIcon } from "./users.svg";
-// export { ReactComponent as GuarantorsIcon } from "./garantor.svg";
-// export { ReactComponent as LoansIcon } from "./loan.svg";
-// export { ReactComponent as DecisionIcon } from "./decisions.svg";
-// export { ReactComponent as SavingsIcon } from "./saving.svg";
-// export { ReactComponent as LoanreqIcon } from "./loan-reqest.svg";
-// export { ReactComponent as WhitelistIcon } from "./whitelist.svg";
-// export { ReactComponent as KarmaIcon } from "./karma.svg";
-// export { ReactComponent as OrganizationIcon } from "./organise.svg";
-// export { ReactComponent as LoanProIcon } from "./loan-product.svg";
-// export { ReactComponent as SavingsProIcon } from "./savings-product.svg";
-// export { ReactComponent as FeesCharIcon } from "./fees.svg";
-// export { ReactComponent as TransactionsIcon } from "./transaction.svg";
-// export { ReactComponent as ServicesIcon } from "./services.svg";
-// export { ReactComponent as ServiceAccIcon } from "./services-acct.svg";
-// export { ReactComponent as SettlementsIcon } from "./settlement.svg";
-// export { ReactComponent as ReportsIcon } from "./report.svg";
-// export { ReactComponent as PreferencesIcon } from "./settings.svg";
-// export { ReactComponent as FeesPriceIcon } from "./pricing.svg";
-// export { ReactComponent as AuditLogsIcon } from "./audit.svg";
-// export { ReactComponent as CaretIcon } from "./caret.svg";
