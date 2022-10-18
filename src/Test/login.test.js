788504c7c9818a520a4b92b22f092008373795fe
Ebu98 <91473642+Ebu98@users.noreply.github.com>
2022-10-18 09:09:10 +0100

    integrated unit testing on the login page

--- src/Test/login.test.js
+++ src/Test/login.test.js
@@ -2,22 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { render, cleanup, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import Login from "../pages/login/login";
+import Login from "../Pages/Login-page/Login"
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-
-const initialState = { users: { data: [] }, auth: {data: []} };
-const mockStore = configureStore();
-let store = mockStore(initialState);
 
 const MockConfirmation = () => {
   return (
-    <Provider store={store}>
       <BrowserRouter>
         <Login />
       </BrowserRouter>
-    </Provider>
   );
 };
 afterEach(cleanup);
@@ -46,8 +38,3 @@ test("renders email input", () => {
   expect(emailElement.value).toBe("test@gmail.com");
 });
 
-test("renders forgot password", () => {
-  render(<MockConfirmation />);
-  const forgotElement = screen.getByText("Welcome!");
-  expect(forgotElement).toBeInTheDocument();
-});
